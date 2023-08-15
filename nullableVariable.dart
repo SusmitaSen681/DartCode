942410de79005c26b0ca202bc8ce21fc12b24c1d
Susmita Sen <76898753+SusmitaSen681@users.noreply.github.com>
2023-08-15 16:09:11 +0530

    Update nullableVariable.dart

--- nullableVariable.dart
+++ nullableVariable.dart
@@ -6,4 +6,10 @@ void main() {
   /*print(name!.length);Uncaught TypeError: Cannot read properties of null (reading 'get$length')
    * Error: TypeError: Cannot read properties of null (reading 'get$length')*/
   print(name?.length);
+  print(name?.length??1);//if name is null then it print 1
+  name="Susmita";
+  //print(name.length);
+  //The property 'length' can't be unconditionally accessed because the receiver can be 'null'
+  print(name!.length);
 }
+
